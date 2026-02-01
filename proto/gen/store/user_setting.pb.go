5d5c2d92164e2ac12ad4ab8d579450f753eda4ff
idunrlylikeu <89637214+idunrlylikeu@users.noreply.github.com>
2026-02-01 08:39:58 -0800

    stuff

--- proto/gen/store/user_setting.pb.go
+++ proto/gen/store/user_setting.pb.go
@@ -239,9 +239,11 @@ type GeneralUserSetting struct {
 	MemoVisibility string `protobuf:"bytes,2,opt,name=memo_visibility,json=memoVisibility,proto3" json:"memo_visibility,omitempty"`
 	// The user's theme preference.
 	// This references a CSS file in the web/public/themes/ directory.
-	Theme         string `protobuf:"bytes,3,opt,name=theme,proto3" json:"theme,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
+	Theme string `protobuf:"bytes,3,opt,name=theme,proto3" json:"theme,omitempty"`
+	// Enable custom date/time when creating memos.
+	EnableCustomMemoDate bool `protobuf:"varint,4,opt,name=enable_custom_memo_date,json=enableCustomMemoDate,proto3" json:"enable_custom_memo_date,omitempty"`
+	unknownFields        protoimpl.UnknownFields
+	sizeCache            protoimpl.SizeCache
 }
 
 func (x *GeneralUserSetting) Reset() {
@@ -293,6 +295,13 @@ func (x *GeneralUserSetting) GetTheme() string {
 		return x.Theme
 	}
 	return ""
+}
+
+func (x *GeneralUserSetting) GetEnableCustomMemoDate() bool {
+	if x != nil {
+		return x.EnableCustomMemoDate
+	}
+	return false
 }
 
 type RefreshTokensUserSetting struct {
@@ -866,11 +875,12 @@ const file_store_user_setting_proto_rawDesc = "" +
 	"\bWEBHOOKS\x10\x05\x12\x12\n" +
 	"\x0eREFRESH_TOKENS\x10\x06\x12\x1a\n" +
 	"\x16PERSONAL_ACCESS_TOKENS\x10\aB\a\n" +
-	"\x05value\"k\n" +
+	"\x05value\"\xa2\x01\n" +
 	"\x12GeneralUserSetting\x12\x16\n" +
 	"\x06locale\x18\x01 \x01(\tR\x06locale\x12'\n" +
 	"\x0fmemo_visibility\x18\x02 \x01(\tR\x0ememoVisibility\x12\x14\n" +
-	"\x05theme\x18\x03 \x01(\tR\x05theme\"\xa4\x04\n" +
+	"\x05theme\x18\x03 \x01(\tR\x05theme\x125\n" +
+	"\x17enable_custom_memo_date\x18\x04 \x01(\bR\x14enableCustomMemoDate\"\xa4\x04\n" +
 	"\x18RefreshTokensUserSetting\x12Y\n" +
 	"\x0erefresh_tokens\x18\x01 \x03(\v22.memos.store.RefreshTokensUserSetting.RefreshTokenR\rrefreshTokens\x1a\x94\x02\n" +
 	"\fRefreshToken\x12\x19\n" +
